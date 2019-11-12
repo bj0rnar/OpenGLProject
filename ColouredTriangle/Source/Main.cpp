6ea44f512872b8613dc9d3310e8aaa56f85bf201
bj0rnar <bj0rnar@hotmail.com>
2019-11-12 11:58:25 +0100

    AWJE

--- ColouredTriangle/Source/Main.cpp
+++ ColouredTriangle/Source/Main.cpp
@@ -86,10 +86,10 @@ GLfloat vertices[] = {
 };
 
 GLfloat box[] = {
-	-5.0f, 5.0f, -10.0f, 0.0f, 4.0f,
+	-5.0f, 5.0f, -10.0f, 0.0f, 1.0f,
 	-5.0f, -5.0f, -10.0f, 0.0f, 0.0f, 
-	5.0f, -5.0f, -10.0f, 4.0f, 0.0f,
-	5.0f, 5.0f, -10.0f, 4.0f, 4.0f, 
+	5.0f, -5.0f, -10.0f, 1.0f, 0.0f,
+	5.0f, 5.0f, -10.0f, 1.0f, 1.0f, 
 };
 
 GLushort boxindices[]{
@@ -392,27 +392,28 @@ int initGL() {
 	// "Bind" the newly created texture : all future texture functions will modify this texture
 	glBindTexture(GL_TEXTURE_2D, textureName);
 
-	// Give an empty image to OpenGL ( the last "0" )
+	// tomt bilde
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1024, 768, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
 
-	// Poor filtering. Needed !
+	// CUSTOMIZE SJØL
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
-	// The depth buffer
+
+	// "render buffer object"
 	glGenRenderbuffers(1, &renderBufferObject);
 	glBindRenderbuffer(GL_RENDERBUFFER, renderBufferObject);
 	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, 1024, 768);
 	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, renderBufferObject);
 
-	// Set "renderedTexture" as our colour attachement #0
+	// COLOR ATTACHMENT
 	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, textureName, 0);
 
-	// Set the list of draw buffers.
+	// draw buffaaaaaahs
 	GLenum DrawBuffers[1] = { GL_COLOR_ATTACHMENT0 };
 	glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
 
-	// Always check that our framebuffer is ok
+	// stjælt sjækk
 	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
 		return false;
 
