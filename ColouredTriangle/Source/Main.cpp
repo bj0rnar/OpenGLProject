1699696e707ee962bb49d1bad855ac4ab48014a5
bj0rnar <bj0rnar@hotmail.com>
2019-11-15 14:15:14 +0100

    r
    r

--- ColouredTriangle/Source/Main.cpp
+++ ColouredTriangle/Source/Main.cpp
@@ -159,6 +159,10 @@ GLfloat *modelMatrixPtr;
 // POINTER FOR BOX
 GLfloat *boxModelMatrixPtr;
 
+//POINTER FOR FBO VIEW & PROJ
+GLfloat *fboProjectionMatrixPtr;
+GLfloat *fboViewMatrixPtr;
+
 // Pointer for light pos
 GLfloat *lightPosPtr;
 
@@ -189,8 +193,15 @@ glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
+//Virtual view 
+glm::mat4 view = glm::mat4(1.0f);
+
+//Fencing
+
+
 //Mouse callback
 void handleMouse(GLFWwindow* window, double xpos, double ypos);
+void setShaders();
 float lastX = DEFAULT_WIDTH / 2.0f;
 float lastY = DEFAULT_HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -198,6 +209,9 @@ float yaw = -90.0f;
 float pitch = 0.0f;
 
 int counter = 0;
+
+//Brukt for å toggle FBO
+bool fboToggle = false;
 
 
 
@@ -254,9 +268,6 @@ int initGL() {
 	// Create and initialize 4 buffer names, DENNA Æ 9 NÅ.
 	glCreateBuffers(11, vertexBufferNames);
 
-	//EGEN BUFFER
-	//glCreateBuffers(1, boxVertexBufferName);
-
 	//NAMED BUFFER STORAGE.
 	glNamedBufferStorage(vertexBufferNames[BOX_VERTICIES], 5 * 4 * 2 *  sizeof(GLfloat), box, 0);
 	glNamedBufferStorage(vertexBufferNames[BOX_INDICES],  6 * 2 * sizeof(GLshort), boxindices, 0);
@@ -267,17 +278,12 @@ int initGL() {
 	// Allocate storage for the triangle indices
 	glNamedBufferStorage(vertexBufferNames[INDICES], 3 * 2 * 6 * sizeof(GLshort), indices, 0);
 
-	//Allocate storage buffer for light position NEW 
-	//glNamedBufferStorage(vertexBufferNames[DYNAMIC_LIGHT], 4 * sizeof(GLfloat), lightPosition, 0);
 
 	// Allocate storage for the transformation matrices and retrieve their addresses
 	glNamedBufferStorage(vertexBufferNames[GLOBAL_MATRICES], 16 * sizeof(GLfloat) * 2, NULL, GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT);
 	glNamedBufferStorage(vertexBufferNames[MODEL_MATRIX], 16 * sizeof(GLfloat), NULL, GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT);
 	
 	glNamedBufferStorage(vertexBufferNames[BOX_MODEL], 16 * sizeof(GLfloat), NULL, GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT);
-
-	//GJØR LYS DYNAMISK.
-	//glNamedBufferStorage(vertexBufferNames[DYNAMIC_LIGHT], 4 * sizeof(GLfloat), NULL, GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT);
 
 	// Allocate storage for the buffers used for lighting calculations ENDRET LIGHT TIL 12
 	glNamedBufferStorage(vertexBufferNames[LIGHT_PROPERTIES], 12 * sizeof(GLfloat), lightProperties, 0);
@@ -346,48 +352,6 @@ int initGL() {
 	glVertexArrayVertexBuffer(boxVertexArray, STREAM0, vertexBufferNames[BOX_VERTICIES], 0, 5 * sizeof(GLfloat));
 
 	//------------------------------RENDER TO TEXTURE ----------------------------------------------------------
-	
-	//_--------------------------------------------LARSTEXTURE----------------------------------------
-	// ------------------------------------------ DETTA FUNKE ----------------------------------------
-	/*
-	//GLubyte *imageData = stbi_load("texture.png", &width, &height, &numChannels, 3);
-	
-	GLubyte *imageData = stbi_load("texture.png", &width, &height, &numChannels, 3);
-	
-	// Generate texture name
-	//Sets the unsigned int to generate a texture ($textureName is just an GLuint)
-	glGenTextures(1, &textureName);
-
-	// Bind the texture
-	// Binds the unsigned int to GL_TEXTURE_2D, OpenGL's library for generating 2d texture
-	glBindTexture(GL_TEXTURE_2D, textureName);
-
-	// Specify the format of the texture
-	// Specifies the actual image data. still targeting the texture above. 
-	// 
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
-
-	// Set the sampler parameters
-	//How to magnifiy the texels
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	//How to minify the the texcels
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	//What happens when you go beyond
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	//Beyond again.
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	// Generate mip maps
-	glGenerateMipmap(GL_TEXTURE_2D);
-
-	// Deactivate texture
-	glBindTexture(GL_TEXTURE_2D, 0);
-	
-	*/
-
-	//_--------------------------------------------LARSTEXTURE----------------------------------------
-	// ------------------------------------------ DETTA FUNKE ----------------------------------------
-	
 
 	// The framebuffer, which regroups 0, 1, or more textures, and 0 or 1 depth buffer.
 	glGenFramebuffers(1, &frameBuffer);
@@ -429,227 +393,6 @@ int initGL() {
 
 
 
-
-	/*
-	// Generate texture name
-	//Sets the unsigned int to generate a texture ($textureName is just an GLuint)
-	glGenTextures(1, &textureName);
-
-	// Bind the texture
-	// Binds the unsigned int to GL_TEXTURE_2D, OpenGL's library for generating 2d texture
-	glBindTexture(GL_TEXTURE_2D, textureName);
-
-	// Specify the format of the texture
-	// Specifies the actual image data. still targeting the texture above. 
-	// 
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
-
-	// Set the sampler parameters
-	//How to magnifiy the texels
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	//How to minify the the texcels
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	//What happens when you go beyond
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	//Beyond again.
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	// Generate mip maps
-	glGenerateMipmap(GL_TEXTURE_2D);
-
-	// Deactivate texture
-	glBindTexture(GL_TEXTURE_2D, 0);
-	
-	//Renderbuffer kanskje ikkje nødvendig?
-
-	//Lag
-	glGenRenderbuffers(1, &renderBufferObject);
-	//Bind
-	glBindRenderbuffer(GL_RENDERBUFFER, renderBufferObject);
-	//Render buffer
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, 1024, 768);
-	//Bind renderBuffer
-	glBindRenderbuffer(GL_RENDERBUFFER, 0);
-
-	// ----------------- FRAMEBUFFER -----------------------
-
-	//Mekk framebuffer
-	glGenFramebuffers(1, &frameBuffer);
-	//BIND FRAMEBUFFER
-	glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
-
-	// BIND FRAMEBUFFER & TEXTURE
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureName, 0);
-
-	//Attach framebuffer & renderbufferobject
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, renderBufferObject);
-
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-	//Viewport
-	//glViewport(0, 0, 1024, 768);
-
-	
-
-	
-	*/
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	/*
-	// ----------------- TEXTURE -----------------------
-	//Lag
-	glGenTextures(1, &textureTest);
-	//Bind
-	glBindTexture(GL_TEXTURE_2D, textureTest);
-	// Tom texture
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1024, 768, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
-	// Unbind
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	// ----------------- RENDER BUFFER OBJECT -----------------------
-
-	//Lag
-	glGenRenderbuffers(1, &renderBufferObject);
-	//Bind
-	glBindRenderbuffer(GL_RENDERBUFFER, renderBufferObject);
-	//Render buffer
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 1024, 768);
-	//Bind renderBuffer
-	glBindRenderbuffer(GL_RENDERBUFFER, 0);
-
-	// ----------------- FRAMEBUFFER -----------------------
-
-	//Mekk framebuffer
-	glGenFramebuffers(1, &frameBuffer);
-	//BIND FRAMEBUFFER
-	glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
-
-	// BIND FRAMEBUFFER & TEXTURE
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureTest, 0);
-	
-	//Attach framebuffer & renderbufferobject
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, renderBufferObject);
-
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-
-	//----------------------- DELETE ?--------------------------
-	glDeleteRenderbuffers(1, &renderBufferObject);
-	glDeleteTextures(1, &textureTest);
-	glDeleteFramebuffers(1, &frameBuffer);
-
-
-
-	/*
-	// mekk framebuffer
-	unsigned int framebuffer;
-	glGenFramebuffers(1, &framebuffer);
-	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-
-	// generate texture
-	unsigned int texColorBuffer;
-	glGenTextures(1, &texColorBuffer);
-	glBindTexture(GL_TEXTURE_2D, texColorBuffer);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1024, 768, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	// attach it to currently bound framebuffer object
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColorBuffer, 0);
-
-
-	//Render buffer object
-	unsigned int rbo;
-	glGenRenderbuffers(1, &rbo);
-	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 1024, 768);
-	glBindRenderbuffer(GL_RENDERBUFFER, 0);
-
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
-
-
-	// DETTA KJØRE, MEN ALT BLI SVART.
-	/*
-	glGenTextures(1, &texture_map);
-	glBindTexture(GL_TEXTURE_2D, texture_map);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1024, 768, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
-
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	// Build the texture that will serve as the depth attachment for the framebuffer.
-	GLuint depth_texture;
-	glGenTextures(1, &depth_texture);
-	glBindTexture(GL_TEXTURE_2D, depth_texture);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, 1024, 768, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, NULL);
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	// Build the framebuffer.
-	GLuint framebuffer;
-	glGenFramebuffers(1, &framebuffer);
-	glBindFramebuffer(GL_FRAMEBUFFER, (GLuint)framebuffer);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture_map, 0);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depth_texture, 0);
-
-	GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-	if (status != GL_FRAMEBUFFER_COMPLETE)
-		// Error
-
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-	*/
-
-	/*
-	GLuint frameBuffer = 0;
-	glGenFramebuffers(1, &frameBuffer);
-	glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
-
-	GLuint renderToTexture;
-	glGenTextures(GL_TEXTURE_2D, &renderToTexture);
-
-	glBindTexture(GL_TEXTURE_2D, renderToTexture);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1024, 768, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
-	GLuint depthrenderbuffer;
-	glGenRenderbuffers(1, &depthrenderbuffer);
-	glBindRenderbuffer(GL_RENDERBUFFER, depthrenderbuffer);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, 1024, 768);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthrenderbuffer);
-
-	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, renderToTexture, 0);
-
-
-	GLenum DrawBuffers[1] = { GL_COLOR_ATTACHMENT0 };
-	glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
-
-	glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
-	glViewport(0, 0, 1024, 768);
-
-	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-		return false;
-		*/
 		//------------------------------RENDER TO TEXTURE ----------------------------------------------------------
 
 	// Load and compile vertex shader
@@ -782,7 +525,7 @@ int initGL() {
  * Draw OpenGL screne
  */
 
-void drawGLScene() {
+void drawGLScene(glm::mat4 view) {
 
 	//glBindFrameBuffer(GL_FRAMEBUFFER, frameBuffer);
 
@@ -792,17 +535,25 @@ void drawGLScene() {
 	// Clear color and depth buffers
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
-	//BIND FRAMEBUFFER HER
-	//glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
 
-	// Set the view matrix
-	glm::mat4 view = glm::mat4(1.0f);
+	// Någgå slekk her tenkte e.
 
-	//LookAt ny metode
-	view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+	//memcpy Viewmatrise
+	//memcpy(viewMatrixPtr, &view[0][0], 16 * sizeof(GLfloat));
+	//memcpy(fboViewMatrixPtr, &view[0][0], 16 * sizeof(GLfloat));
+
 	memcpy(viewMatrixPtr, &view[0][0], 16 * sizeof(GLfloat));
 
+	//memcpy(fboViewMatrixPtr, &view[0][0], 16 * sizeof(GLfloat));
 
+	/*
+	if (fboToggle) {
+		memcpy(fboViewMatrixPtr, &view[0][0], 16 * sizeof(GLfloat));
+	}
+	else {
+		memcpy(viewMatrixPtr, &view[0][0], 16 * sizeof(GLfloat));
+	}
+	*/
 	//LIght
 
 	//PsuedoGravity
@@ -813,53 +564,6 @@ void drawGLScene() {
 	if (cameraPos.y < -19.0f) {
 		cameraPos.y += 0.1f;
 	}
-	/*
-	//Red warp to green
-	if (cameraPos.z <= -19.0f) {
-		cameraPos.z = 0.0f;
-		cameraPos.x = 19.0f;
-
-		glm::mat4 rotateCamera = glm::mat4(1.0f);
-		rotateCamera = glm::rotate(rotateCamera, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-
-		cameraFront = glm::mat3(rotateCamera) * cameraFront;
-	}
-
-
-	//Black warp to top
-	if (cameraPos.z >= 19.0f) {
-		cameraPos.x = 0.0f;
-		cameraPos.z = 0.0f;
-		cameraPos.y = 100.0f;
-
-		/*
-		glm::mat4 turnCameraDown = glm::mat4(1.0f);
-		turnCameraDown = glm::rotate(turnCameraDown, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-		
-		cameraFront = glm::mat3(turnCameraDown) * cameraFront;
-		*/
-	//}
-
-	
-
-	/*
-	if (cameraPos.z >= 19.0f) {
-		cameraPos.z = -19.0f;
-	}
-	/*
-	if (cameraPos.x <= -19.0f) {
-		cameraPos.x = 19.0f;
-	}
-
-	if (cameraPos.x >= 19.0f) {
-		cameraPos.x = -19.0f;
-	}
-	
-	//glBufferData (something something, GL_STATIC_DRAW) versjon 3.3
-	kan heller bruke glNamedBufferStorage(vertexBufferNames[MODEL_BOX]) nåvernende versjon.
-
-
-	*/
 	
 	counter++;
 
@@ -879,11 +583,6 @@ void drawGLScene() {
 	lightP = lightM * lightP;
 	memcpy(lightPosPtr, &lightP, 16 * sizeof(float));
 
-	//memcpy(lightPosPtr, &light[0][0], 4 * sizeof(GLfloat));
-
-
-	//Model treng ikkje rotetere, kommentere ut detta.
-	// Set the model matrix
 	
 	GLfloat scale[16] = {
 		1, 0, 0, 0,
@@ -900,77 +599,6 @@ void drawGLScene() {
 		0, 0, 0, 1 };
 	memcpy(boxModelMatrixPtr, test, 16 * sizeof(GLfloat));
 	
-	
-	/*
-	glm::mat4 model = glm::mat4(1.0f);
-	model = glm::rotate(model, (float)glfwGetTime() * 0.3f, glm::vec3(0.0f, -1.0f, 0.0f));
-	memcpy(modelMatrixPtr, &model[0][0], 16 * sizeof(GLfloat));
-	*/
-
-	//lightProperties[0] = 2.0f;
-	//cameraProperties[0] = 4.0f;
-
-	//lightProperties[0] = 1.0f;
-
-
-
-	//ALL THIS STUFF.
-	/**/
-
-	/*
-	float lightX = 2.0f * sin(glfwGetTime());
-	float lightY = -0.3f;
-	float lightZ = 1.5f * cos(glfwGetTime());
-
-	glm::vec3 lightPos = glm::vec3(lightX, lightY, lightZ);
-
-	memcpy(lightPosPtr, &lightPos, 4 * sizeof(GLfloat));
-	*/
-
-
-
-
-	//Not working
-	//glm::mat4 light = glm::mat4(1.0);
-	//light = glm::rotate(light, (float)glfwGetTime() * 2.0f, glm::vec3(5.0f, 5.0f, 5.0f));
-	//memcpy(lightPosPtr, &light[0][0], 4 * sizeof(GLfloat));
-
-	//glUniform3f(LIGHT, lightX, lightY, lightZ);
-
-
-
-	/*
-
-
-	glm::vec4 lightPos(LIGHT);
-
-	lightPos.x = 1.0f + sin(glfwGetTime()) * 2.0f;
-	lightPos.y = sin(glfwGetTime() / 2.0f) * 1.0f;
-
-
-	glm::mat4 lightRotate(1.0f);
-	lightRotate = glm::rotate(lightRotate, lightPos.x, glm::vec3(1.0f, 0.0f, 0.0f));
-	lightRotate = glm::rotate(lightRotate, lightPos.y, glm::vec3(0.0f, 1.0f, 0.0f));
-
-	glm::vec4 rotatedLightPosition = lightRotate * lightPos;
-
-	glUniform3f(LIGHT, rotatedLightPosition.x, rotatedLightPosition.y, rotatedLightPosition.z);
-	*/
-
-	//Sett 1:1 scale, denna ken stå statisk.
-
-
-	/*
-	GLfloat scale[16] = {
-		1, 0, 0, 0,
-		0, 1, 0, 0,
-		0, 0, 1, 0,
-		0, 0, 0, 1 };
-	memcpy(modelMatrixPtr, scale, 16 * sizeof(GLfloat));
-	*/
-	// Activate the program
-
-	//glBindBuffer(GL_FRAMEBUFFER, 0);
 
 	glUseProgram(programName);
 
@@ -980,29 +608,22 @@ void drawGLScene() {
 	glBindVertexArray(vertexArrayName);
 	
 	
+	
 	// Bind buffers to GLSL uniform indices
 	glBindBufferBase(GL_UNIFORM_BUFFER, TRANSFORM0, vertexBufferNames[GLOBAL_MATRICES]);
+	//glBindBufferBase(GL_UNIFORM_BUFFER, TRANSFORM0, vertexBufferNames[GLOBAL_MATRICES]);
 	glBindBufferBase(GL_UNIFORM_BUFFER, TRANSFORM1, vertexBufferNames[MODEL_MATRIX]);
 	glBindBufferBase(GL_UNIFORM_BUFFER, LIGHT, vertexBufferNames[LIGHT_PROPERTIES]);
 	glBindBufferBase(GL_UNIFORM_BUFFER, MATERIAL, vertexBufferNames[MATERIAL_PROPERTIES]);
 	glBindBufferBase(GL_UNIFORM_BUFFER, CAMERA, vertexBufferNames[CAMERA_PROPERTIES]);
 	//BIND TO GLSL
 	glBindBufferBase(GL_UNIFORM_BUFFER, DYNAMICLIGHT, vertexBufferNames[DYNAMIC_LIGHT]);
-	
-
 
 	// Draw første gang
 	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, 0);
 	
-	//glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
 	glUseProgram(0);
-
-
-	//glBindBufferBase(GL_UNIFORM_BUFFER, TRANSFORM1, 0);
-
-
-	//glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
 
 	
 	glUseProgram(renderProgram);
@@ -1012,13 +633,7 @@ void drawGLScene() {
 	glBindVertexArray(boxVertexArray);
 	glBindTexture(GL_TEXTURE_2D, textureName);
 
-	//Draw neste model
 
-
-
-	//TRENGS DENNA?
-	//glBindBufferBase(GL_UNIFORM_BUFFER, TRANSFORM0, vertexBufferNames[GLOBAL_MATRICES]);
-	//glBindBufferBase(GL_UNIFORM_BUFFER, TRANSFORM1, vertexBufferNames[BOX_MODEL]);
 
 
 	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, 0);
@@ -1026,12 +641,12 @@ void drawGLScene() {
 	
 
 	// Disable
-	//glUseProgram(0);
-	//glBindTexture(GL_TEXTURE_2D, 0);
-	//glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glUseProgram(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
 	glBindVertexArray(0);
 	
 }
+
 
 void handleButtons(GLFWwindow *window) {
 
@@ -1144,44 +759,6 @@ int main(void) {
 		exit(EXIT_FAILURE);
 	}
 
-	/*	NOTAT TE KAMERA OBLIG
-
-		glm::vec3 cameraPosition;
-		glm::vec3 cameraForward;
-		glm::vec3 cameraLeft;
-
-		i main funksjonen:
-		glfwSetKeyCallback <-- håndterer input
-		keyevents æ bæd å laggy
-		keycallback æ insta.
-		key input > text input
-
-		noe typ detta for tastatur:
-
-		int movingForward = 0
-
-		if(key == GLFW_KEY_W && action == GLFW_PRESS) {
-			movingForward = 1
-		}
-
-		for å gjøre selve utregninga i render:
-		Framover:
-		cameraPos += cameraForward * speed * timeElapsed
-
-		noe typ detta for mus:
-
-		sjekk raw mouse motion ell cursor position
-		bruker en av de for å hente ut X og Y coordinater til mus.
-
-		OBS: trenger ikke noe ny kode på GPU siden, alt av input av keyboard/mus skjer på CPU siden.
-
-		For kameragreier, google linear interpolation.
-
-		Quaternion.
-
-	*/
-
-
 
 	// Set input key event callback
 	glfwSetKeyCallback(window, glfwKeyCallback);
@@ -1225,14 +802,23 @@ int main(void) {
 		glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
 		glViewport(0, 0, 1024, 768);
 
-		// Draw OpenGL screne
-		drawGLScene();
+		//Draw in FBO
+		//view = glm::lookAt(glm::vec3(-5.0f, -10.0f, -19.0f), glm::vec3(0.0f, 0.0f, -19.0f), cameraUp);
+		drawGLScene(view);
+
+		//FENCING???
+		glFenceSync(GL_SYNC_GPU_COMMANDS_COMPLETE, 0);
 
 		//FJERN FØR E KJØM TE SWAP BUFFER
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 		glViewport(0, 0, 1024, 768);
 
-		drawGLScene();
+		//Draw IRL
+		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+		drawGLScene(view);
+
+
+		glFenceSync(GL_SYNC_GPU_COMMANDS_COMPLETE, 0);
 
 		// ACtivate buttons!
 		handleButtons(window);
